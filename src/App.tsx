06dc486290257fc31ea954988ab911b1dc3e84bc
panakota <dimapanasiuk@icloud.com>
2021-06-02 18:48:14 +0300

    feat: add registration UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,58 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { useMutation, useQuery } from '@apollo/client';
 import { GET_ALL_USERS } from './query/user';
 import { CREATE_USER } from './mutations';
+import Layout from './components/Layout';
 
 const App: React.FC = () => {
-  const { data, loading, error } = useQuery(GET_ALL_USERS);
-
-  const [newUser] = useMutation(CREATE_USER)
-  const [users, setUsers] = useState<any>([])
-  const [username, setUsername] = useState<string>('')
-  const [password, setPassword] = useState<string>('')
-  const [email, setEmail] = useState<string>('')
-
-  const [age, setAge] = useState<number>(0)
-
-
-  useEffect(() => {
-    if (!loading) {
-      setUsers(data.findAllUser)
-    }
-  }, [data]);
-
-  const addUser = (e: any) => {
-    e.preventDefault()
-    newUser({
-        variables: {
-            input: {
-                username, age, password, email
-            }
-        }
-    }).then(({data}) => {
-        console.log(data)
-        setUsername('');
-        setAge(0);
-        setEmail('');
-        setPassword('');
-    })
-}
-
   return (
     <>
-      <form action="">
-        <input type="text" value ={username} placeholder='username' onChange={e => setUsername(e.target.value)}/>
-        <input type="text" value ={password} placeholder='password' onChange={e => setPassword(e.target.value)}/> 
-        <input type="text" value ={email} placeholder='email' onChange={e => setEmail(e.target.value)}/> 
-        <input type="number" value ={age} onChange={e => setAge(+e.target.value)}/>
-
-        <div>
-          <button onClick={e=> addUser(e)}>Create</button>
-          <button>Get Users</button>
-        </div>
-      </form>
-      <div>
-        {users.map((i: { name: string; }) => <h1>{i.name}</h1>)}
-      </div>
+      < Layout />
     </>
   )
 }
---
+++ src/components/RegistrationModal/index.tsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import { Modal, Button } from 'antd';
+
+import { useMutation, useQuery } from '@apollo/client';
+import { GET_ALL_USERS } from '../../query/user';
+import { CREATE_USER } from '../../mutations';
+
+const RegistrationModal = () => {
+    const { data, loading, error } = useQuery(GET_ALL_USERS);
+
+    const [newUser] = useMutation(CREATE_USER)
+    const [users, setUsers] = useState<any>([])
+    const [username, setUsername] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const [email, setEmail] = useState<string>('')
+  
+    const [age, setAge] = useState<number>(0)
+  
+  
+    useEffect(() => {
+      if (!loading) {
+        // setUsers(data.findAllUser)
+      }
+    }, [data]);
+  
+    const addUser = (e: any) => {
+      e.preventDefault()
+      newUser({
+          variables: {
+              input: {
+                  username, age, password, email
+              }
+          }
+      }).then(({data}) => {
+          console.log(data)
+          setUsername('');
+          setAge(0);
+          setEmail('');
+          setPassword('');
+      })
+  }
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <>
+      <Button type="primary" onClick={showModal}>
+        Registration
+      </Button>
+
+      <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+      <form action="">
+        <input type="text" value ={username} placeholder='username' onChange={e => setUsername(e.target.value)}/>
+        <input type="text" value ={password} placeholder='password' onChange={e => setPassword(e.target.value)}/> 
+        <input type="text" value ={email} placeholder='email' onChange={e => setEmail(e.target.value)}/> 
+        <input type="number" value ={age} onChange={e => setAge(+e.target.value)}/>
+
+        <div>
+          <button onClick={e=> addUser(e)}>Create</button>
+          <button>Get Users</button>
+        </div>
+      </form>
+      <div>
+        {users.map((i: { name: string; }) => <h1>{i.name}</h1>)}
+      </div>
+      </Modal>
+    </>
+  );
+};
+
+export default RegistrationModal;
