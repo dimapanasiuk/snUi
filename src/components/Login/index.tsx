5128a0e70d3602aaad04a36a6e04581bb40c3c05
panakota <dimapanasiuk@icloud.com>
2021-06-17 13:30:13 +0300

    refactor: login refactoring

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,49 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Modal, Button } from 'antd';
-import { connect, useDispatch  } from "react-redux"; 
+import { useDispatch } from "react-redux";
 import { useQuery } from '@apollo/client';
 import { GET_LOGIN_INFO } from '../../query/user';
-import {loginAc} from "../../redux/actions/loginActions";
 import { LOGIN_AC } from "../../redux/constants/counterConstants";
 
-const LoginModal = ({loginData}: any) => {
-  const [username, setUsername] = useState<string>(loginData?.username);
-  const [password, setPassword] = useState<string>(loginData?.password);
+const LoginModal = () => {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
 
-  const [dataLogin, setDataLogin  ] =useState<any>({
-    id: "",
-    username: "",
-    password: ""});
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
-
-  const isLogin = !!(loginData?.username && loginData?.password || dataLogin?.username && dataLogin?.password);
-
-  const {data, loading} = useQuery(GET_LOGIN_INFO, {
+  const { data: lolData, loading } = useQuery(GET_LOGIN_INFO, {
     variables: {
       input: {
-        // username: dataLogin?.username,
-        // password: dataLogin?.password
-        username: 'test',
-        password: 'test',
+        username, password
       }
     }
   });
 
-  useEffect(() => {
-    if (!loading) {
-      // console.log(data);
-    }
-  }, [data]);
-  
   const loginHandler = (e: any) => {
     e.preventDefault();
-    const data = {username, password, email: ''};
-    setDataLogin(data);
-    dispatch({type: LOGIN_AC, data});
+    const data = { username, password, email: '' };
+    console.log(lolData?.getLoginData);
+    if (lolData?.getLoginData) {
+      dispatch({ type: LOGIN_AC, data });
+    }
   }
-
-  const [isModalVisible, setIsModalVisible] = useState(false);
 
   const showModal = () => {
     setIsModalVisible(true);
@@ -60,29 +44,21 @@ const LoginModal = ({loginData}: any) => {
   return (
     <>
       <Button type="primary" onClick={showModal}>
-        {isLogin ? (loginData?.username || username) : 'login'}
+        {lolData?.getLoginData ? (lolData?.getLoginData?.username || username) : 'login'}
       </Button>
 
       <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-      <form action="">
-        <input type="text" value ={username} placeholder='username' onChange={e => setUsername(e.target.value)}/>
-        <input type="text" value ={password} placeholder='password' onChange={e => setPassword(e.target.value)}/> 
+        <form action="">
+          <input type="text" value={username} placeholder='username' onChange={e => setUsername(e.target.value)} />
+          <input type="text" value={password} placeholder='password' onChange={e => setPassword(e.target.value)} />
 
-        <div>
-          <button onClick={e=> loginHandler(e)}>Login</button>
-        </div>
-      </form>
+          <div>
+            <button onClick={e => loginHandler(e)}>Login</button>
+          </div>
+        </form>
       </Modal>
     </>
   );
 };
 
-const mapStateToProps = (state: any) => {
-  return  {
-  loginData: state.login,
- }
-};
- 
- const mapDispatchToProps = { loginAc };
-
-export default connect(mapStateToProps, mapDispatchToProps)(LoginModal);
+export default LoginModal;
